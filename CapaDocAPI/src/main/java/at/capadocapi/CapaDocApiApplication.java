247ba8699fad63dfd732d42c8d2d344afb2535ff
Pascal Letsch <pascal.letsch2002@gmail.com>
2026-09-19 20:00:18 +0200

    feat: enhance database configuration with default values

--- CapaDocAPI/src/main/java/at/capadocapi/CapaDocApiApplication.java
+++ CapaDocAPI/src/main/java/at/capadocapi/CapaDocApiApplication.java
@@ -8,6 +8,9 @@ public class CapaDocApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CapaDocApiApplication.class, args);
+        for (int i = 0; i < 10; i++) {
+        System.out.println("Hello World");
+        }
     }
 
 }
